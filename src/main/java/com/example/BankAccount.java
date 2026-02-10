40adb9604d05198624453f11d64170b2cacfaa18
gamaleev00-art <gamaleev.00@gmail.com>
2026-02-10 14:52:42 +0300

    Создал ConcurrentBank

--- src/main/java/com/example/BankAccount.java
+++ src/main/java/com/example/BankAccount.java
@@ -1,4 +1,32 @@
 package com.example;
 
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class BankAccount {
+    private final int id;
+    private BigDecimal balance;
+
+    public BankAccount(int id,BigDecimal balance) {
+        this.balance = balance;
+        this.id = id;
+    }
+
+    public synchronized void deposit(BigDecimal amount) {
+        balance = balance.add(amount);
+    }
+
+    public synchronized boolean withdraw(BigDecimal amount) {
+        if (balance.compareTo(amount) >= 0) {
+            balance = balance.subtract(amount);
+            return true;
+        } else {
+            System.out.println("Недостаточно средств");
+            return false;
+        }
+    }
+
+    public synchronized BigDecimal getBalance() {
+        return balance;
+    }
 }
--- src/main/java/com/example/ConcurrentBank.java
+++ src/main/java/com/example/ConcurrentBank.java
@@ -1,4 +1,36 @@
 package com.example;
 
+import java.math.BigDecimal;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ConcurrentBank {
+
+    private ConcurrentHashMap<Integer, BankAccount> accounts = new ConcurrentHashMap<>();
+    AtomicInteger idGenerator = new AtomicInteger(0);
+
+    public BankAccount createAccount(BigDecimal balance) {
+        int id = idGenerator.incrementAndGet();
+        BankAccount bankAccount = new BankAccount(id, balance);
+        accounts.put(id, bankAccount);
+        return bankAccount;
+    }
+
+    public void transfer(BankAccount fromAccount, BankAccount toAccount, BigDecimal amount) {
+        synchronized (fromAccount) {
+            synchronized (toAccount) {
+                if (fromAccount.withdraw(amount)) {
+                    toAccount.deposit(amount);
+                }
+            }
+        }
+    }
+
+    public BigDecimal getTotalBalance() {
+        synchronized (this) {
+            return accounts.values().stream()
+                    .map(BankAccount::getBalance)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+        }
+    }
 }
